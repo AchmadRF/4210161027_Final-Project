b8bcf40a2e84d5fcba5d4acdca0b126eebd99c39
Arofu10 <frozt10@gmail.com>
2019-05-29 12:20:46 +0700

    Establish Connection

---
+++ Panah Arjuna Project/Assets/Script/Launcher.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Launcher : MonoBehaviourPunCallbacks {
+
+	// Use this for initialization
+	void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+	
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+    }
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        //Terserah;
+    }
+    public override void OnJoinedLobby()
+    {
+        //Terserah
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Panah Arjuna Project/Assets/Script/RoomMaker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
+using UnityEngine.UI;
+
+public class RoomMaker : MonoBehaviourPunCallbacks {
+    public InputField RoomName;
+    public InputField JoinRoom;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    public void Make_Join()
+    {
+        PhotonNetwork.JoinRoom(JoinRoom.text, null);
+    }
+
+    public void Create_ToJoin()
+    {
+        PhotonNetwork.CreateRoom(RoomName.text, new RoomOptions { MaxPlayers = 2 }, null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel(1);
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        print("Failed To Join" + returnCode + "Message" + message);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
