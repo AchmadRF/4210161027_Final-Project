8b4f30a1fbee2169446f05f31abbdba62bab2104
Arofu10 <frozt10@gmail.com>
2019-06-18 13:26:06 +0700

    Adding player sprite and core mechanic also starting photon synchronize

---
+++ Assets/Script/Manager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Manager : MonoBehaviour {
+
+    public GameObject PlayerPref;
+    public GameObject arrow;
+    public Transform SpawnPoints;
+    public Transform SpawnPoints2;
+	// Use this for initialization
+	void Start () {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            SpawnPlayer();
+        }
+        else
+        {
+            SpawnPlayer2();
+        }
+	}
+
+    void SpawnPlayer()
+    {
+        PhotonNetwork.Instantiate(PlayerPref.name, SpawnPoints.position, Quaternion.identity);
+    }
+
+    void SpawnPlayer2()
+    {
+        PhotonNetwork.Instantiate(PlayerPref.name, SpawnPoints2.position, Quaternion.identity);
+    }
+
+    void Fire()
+    {
+        GameObject chars = GameObject.Find("Chars(Clone)");
+        UnityEngine.UI.Slider slider = FindObjectOfType<UnityEngine.UI.Slider>();
+        GameObject arr = Instantiate(arrow, chars.GetComponentsInChildren<Transform>()[0].transform.position, Quaternion.Euler(0, 0, slider.value));
+        arr.GetComponent<Rigidbody2D>().velocity = new Vector2(5, 5);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
+    }
+}
---
+++ Assets/Script/Shoot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shoot : MonoBehaviour {
+
+    public GameObject Pivot;
+    public Slider slider;
+    public float Trajectory = 10f;
+	// Use this for initialization
+	void Start () {
+        //Pivot = GameObject.Find("Chars");
+        slider = FindObjectOfType<Slider>();
+	}
+	
+    void FixedUpdate()
+    {
+        transform.localRotation = Quaternion.Euler(0, 0, slider.value);
+        //transform.RotateAround(Pivot.transform.localPosition, Vector3.back, Time.deltaTime * Trajectory);
+    }
+
+}
